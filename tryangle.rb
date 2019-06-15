9de897bcd035cf129b80ab250ed61b905cb91b9c
noei <logmanxsa@gmail.com>
2019-06-15 12:57:28 +0700

    change varible name

--- tryangle.rb
+++ tryangle.rb
@@ -2,13 +2,13 @@ row = 10
 
 for i in 1..row
 
-  no_start = (i * 2) - 1
+  spec = (i * 2) - 1
 
   for j in i..row
     print " "
   end
 
-  for x in no_start.downto(1)
+  for x in spec.downto(1)
     print "*"
   end
   
